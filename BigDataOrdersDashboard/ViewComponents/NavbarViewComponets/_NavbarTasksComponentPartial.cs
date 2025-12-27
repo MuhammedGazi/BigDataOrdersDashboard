07d8e11e7725702d899ffb953602ddd8583c4ca4
MuhammedGazi <muhammed.celenk1@ogr.sakarya.edu.tr>
2025-12-27 14:40:25 +0300

    Add project files.

---
+++ BigDataOrdersDashboard/ViewComponents/NavbarViewComponets/_NavbarTasksComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BigDataOrdersDashboard.ViewComponents.NavbarViewComponets
+{
+    public class _NavbarTasksComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
