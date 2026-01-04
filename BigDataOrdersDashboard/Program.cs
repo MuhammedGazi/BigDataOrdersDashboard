a26fbdda43c0fe2561168e1ccd43e93e70f175df
MuhammedGazi <muhammed.celenk1@ogr.sakarya.edu.tr>
2026-01-04 16:27:40 +0300

    ml.net ile forecast

--- BigDataOrdersDashboard/Program.cs
+++ BigDataOrdersDashboard/Program.cs
@@ -1,5 +1,6 @@
 using BigDataOrdersDashboard.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.ML;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,8 @@ builder.Services.AddDbContext<BigDataOrdersDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<MLContext>();
 
 var app = builder.Build();
 
